c7fd7e15695b1cb1f60eaa56dbb4a4091cb86532
rfloeter <rainer.r.floeter@gmail.com>
2019-04-03 19:29:33 -0400

    Updates

---
+++ precache-manifest.fd74f12f806b8aa770292020d84bbb3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "46fd638e1edb54ea5b52",
+    "url": "/my-app/static/js/main.a75c98f8.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/my-app/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "46fd638e1edb54ea5b52",
+    "url": "/my-app/static/css/main.36016d6d.chunk.css"
+  },
+  {
+    "revision": "9893568bbd2ecc8e7215b96a338c4010",
+    "url": "/my-app/index.html"
+  }
+];
--- src/Container.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import CSS from "./container.css"
-
-const Container = props =>
-  <div className={`Container${props.fluid ? "-fluid" : ""}`}>
-    {props.children}
-  </div>;
-
-export default Container;
